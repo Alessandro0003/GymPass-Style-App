ad1dd9151d6d14ce4a0781dbacca08df8b477827
Alessandro0003 <alessandroquinta3@gmail.com>
2024-01-16 23:03:55 -0300

    :wrench: chore: Addings settings in environment variables to the application

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import { app } from './app'
+import { env } from './env'
 
-const PORT = 3333
+
 
 app.listen({
     host: '0.0.0.0',
-    port: PORT,
+    port: env.PORT 
 }).then(() => {
-    console.log(`🚀 HTTP Server Running in http://localhost:${PORT}`)
+    console.log(`🚀 HTTP Server Running in http://localhost:${env.PORT}`)
 })
