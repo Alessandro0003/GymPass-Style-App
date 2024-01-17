f3710554fb2939390973aea2ecb34d8af1881749
Alessandro0003 <alessandroquinta3@gmail.com>
2024-01-17 12:26:18 -0300

    :sparkles: feat: creating RegisterUseCasesRequest in to create users

---
+++ src/http/controllers/register.ts
@@ -0,0 +1,25 @@
+import { z } from 'zod'
+import { type FastifyRequest, type FastifyReply } from 'fastify'
+import { registerUseCases } from '@/use-cases/register'
+
+export async function Register(request: FastifyRequest, reply: FastifyReply) {
+  const createUserBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
+
+  const { name, email, password } = createUserBodySchema.parse(request.body)
+
+  try {
+    await registerUseCases({
+      name,
+      email,
+      password,
+    })
+  } catch (err) {
+    return reply.status(409).send()
+  }
+
+  return reply.status(201).send()
+}
