bd4b09079bd63bedeca918c3c15f54d4883ad5f3
Alessandro0003 <alessandroquinta3@gmail.com>
2024-01-20 19:52:03 -0300

    :sparkles: feat: creating route athentication

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,6 +1,9 @@
 import { FastifyInstance } from 'fastify'
 import { Register } from './controllers/register'
+import { Authentication } from './controllers/authentication'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', Register)
+
+  app.post('/sessions', Authentication)
 }
