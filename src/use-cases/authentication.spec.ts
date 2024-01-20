9d80af19a66cd45ffca7bd6a7a6ec3bd14dee5fb
Alessandro0003 <alessandroquinta3@gmail.com>
2024-01-20 19:54:44 -0300

    :test_tube: test: ensure AuthenticationUseCase is valid authentication users

---
+++ src/use-cases/authentication.spec.ts
@@ -0,0 +1,54 @@
+import { expect, it, describe, beforeEach } from 'vitest'
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
+import { AuthenticationUseCase } from './authentication/authentication'
+import { hash } from 'bcryptjs'
+import { InvalidCredentialsError } from './errors/invalid-credentials-error'
+
+let usersRepository: InMemoryUsersRepository
+let sut : AuthenticationUseCase
+
+describe('Authentication Use Case', () => {
+
+    beforeEach(() => {
+        usersRepository = new InMemoryUsersRepository()
+        sut = new AuthenticationUseCase(usersRepository)
+    })
+
+    it('should be able to authentication', async () => {
+        
+        await usersRepository.create({
+            name: 'any_name',
+            email: 'any_mail@email.com',
+            password_hash: await hash('any_password', 6)
+        })
+
+        const { user } =  await sut.execute({
+            email: 'any_mail@email.com',
+            password: 'any_password',
+        })
+
+        expect(user.id).toEqual(expect.any(String))
+    })
+
+    it('should not be able to authentication with wrong email', async () => {
+
+        expect(() => sut.execute({
+            email: 'any_mail@email.com',
+            password: 'any_password',
+        })).rejects.toBeInstanceOf(InvalidCredentialsError)
+    })
+
+    it('should not be able to authentication with wrong password', async () => {
+
+        await usersRepository.create({
+            name: 'any_name',
+            email: 'any_mail@email.com',
+            password_hash: await hash('any_password', 6)
+        })
+
+        expect(() => sut.execute({
+            email: 'any_mail@email.com',
+            password: 'any_passwords',
+        })).rejects.toBeInstanceOf(InvalidCredentialsError)
+    })
+})
