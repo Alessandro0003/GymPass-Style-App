5cedd37abf8cd158c2c10934806379ffbb8c6f25
Alessandro0003 <alessandroquinta3@gmail.com>
2024-01-21 00:33:05 -0300

    :sparkles: feat: ensure GymRepository creating is gym

---
+++ src/repositories/in-memory/in-memory-gyms-repository.ts
@@ -0,0 +1,20 @@
+import { Gym } from '@prisma/client'
+import { GymsRepository } from '../gyms/gyms-repository'
+    
+
+export class InMemoryGymsRepository implements GymsRepository { 
+
+   public items: Gym[] = []
+
+   async  findById(id: string) {
+      const gym = this.items.find((item) => item.id === id)
+
+      if (!gym) {
+        return null
+      }
+
+      return gym
+   }
+
+   
+}
