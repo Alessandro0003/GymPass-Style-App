89d89e0899b36ffb594948b151252e2e2290c409
Alessandro0003 <alessandroquinta3@gmail.com>
2024-01-20 21:01:05 -0300

    :sparkles: feat: ensure findById return is Id to user

--- src/repositories/in-memory/in-memory-users-repository.ts
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -2,8 +2,20 @@ import { Prisma, User } from '@prisma/client'
 import { UsersRepository } from '../users/users-repository'
     
 
-export class InMemoryUsersRepository implements UsersRepository {
+export class InMemoryUsersRepository implements UsersRepository { 
+
    public items: User[] = []
+
+   async  findById(id: string) {
+      const user = this.items.find((item) => item.id === id)
+
+      if (!user) {
+        return null
+      }
+
+      return user
+   }
+
 
    async  findByEmail(email: string) {
       const user = this.items.find((item) => item.email === email)
