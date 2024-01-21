3d288242f1f38abc56e8dfaf1d11f323721019cb
Alessandro0003 <alessandroquinta3@gmail.com>
2024-01-20 23:50:40 -0300

    :test_tube: test: ensure CheckInUseCase return throw error is check-in for duplicated

--- src/use-cases/check-in.spec.ts
+++ src/use-cases/check-in.spec.ts
@@ -1,4 +1,4 @@
-import { expect, it, describe, beforeEach } from 'vitest'
+import { expect, it, describe, beforeEach, vi, afterEach} from 'vitest'
 import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository'
 import { CheckInUseCase } from './check-in/check-in'
 
@@ -10,6 +10,12 @@ describe('Check-in Use Case', () => {
     beforeEach(() => {
         checkInsRepository = new InMemoryCheckInsRepository()
         sut = new CheckInUseCase( checkInsRepository)
+
+        vi.useFakeTimers()
+    })
+
+    afterEach(() => {
+        vi.useRealTimers()
     })
 
     it('should be able to check in', async () => {
@@ -20,5 +26,19 @@ describe('Check-in Use Case', () => {
         })
 
         expect(checkIn.id).toEqual(expect.any(String))
+    })
+
+    it('should not be able to check in twice in the same day', async () => {
+        vi.setSystemTime(new Date(2024, 0, 20, 23, 36, 0))
+
+        await sut.execute({
+            gymId: 'any_gym',
+            userId: 'any_user',
+        })
+
+        await expect(() => sut.execute({
+            gymId: 'any_gym',
+            userId: 'any_user',
+        })).rejects.toBeInstanceOf(Error)
     })
 })
