98bf1224bbfd7ee2f6b4583b3cdc214df4bc9da1
Alessandro0003 <alessandroquinta3@gmail.com>
2024-01-21 02:18:58 -0300

    :sparkles: feat: ensure is user to fetch check-in history

--- src/repositories/checkin/check-ins-repository.ts
+++ src/repositories/checkin/check-ins-repository.ts
@@ -1,6 +1,8 @@
 import { CheckIn, Prisma } from "@prisma/client";
 
 export interface CheckInsRepository {
-    create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>
     findByUserIdOnDate(userId: string, date: Date): Promise<CheckIn | null>
+    findManyByUserId(userId: string):Promise<CheckIn[]>
+    create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>
+    
 }
