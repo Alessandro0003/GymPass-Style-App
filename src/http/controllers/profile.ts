67808d0448a469105b028c10b578dcf94462c95a
Alessandro0003 <alessandroquinta3@gmail.com>
2024-01-27 00:56:25 -0300

    :saprkles: feat: ensure Profile return is user in token passed in request

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -1,9 +1,20 @@
+import { makeGetUserProfileUseCase } from '@/use-cases/factories/make-get-user-profile-use-case'
 import { FastifyRequest, FastifyReply } from 'fastify'
 
 export async function Profile(request: FastifyRequest, reply: FastifyReply) {
   await request.jwtVerify()
 
-  console.log(request.user.sub)
-  return await reply.status(200).send()
+  const getUserProfile = makeGetUserProfileUseCase()
+
+  const { user } = await getUserProfile.execute({
+    userId: request.user.sub
+  })
+
+  return await reply.status(200).send({
+    user: {
+      ...user,
+      password_hash: undefined,
+    }
+  })
 }
 
