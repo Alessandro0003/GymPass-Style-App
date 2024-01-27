66bf369f6a3fe3195b678b586fe610e5ff0741a8
Alessandro0003 <alessandroquinta3@gmail.com>
2024-01-27 03:03:22 -0300

    :test_tube: testE2E: ensure that token authentication is return when the user login

--- tests/E2E/authenticatoin.spec.ts
+++ tests/E2E/authenticatoin.spec.ts
@@ -1,3 +1,33 @@
-import { test } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import { strict } from 'assert'
 
-test('ok', () =>{})
+describe('Authentication (e2e)', () => {
+    beforeAll(async () => {
+        await app.ready()
+    })
+
+
+    afterAll(async () => {
+        await app.close()
+    })
+
+    it('should be able to authentication', async () => {
+        await request(app.server).post('/users') .send({
+            name: 'Ale Brilhante',
+            email: 'ale@example.com',
+            password: '123456',
+        })
+
+        const response = await request(app.server).post('/sessions').send({
+            email: 'ale@example.com',
+            password: '123456',
+        })
+
+        expect(response.statusCode).toEqual(200)
+        expect(response.body).toEqual({
+            token: expect.any(String)
+        })
+    })
+})
