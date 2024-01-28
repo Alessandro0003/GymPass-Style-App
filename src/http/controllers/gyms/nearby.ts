e9f9474b08d5f6bb7aa0692b26b58adb9c46f8f4
Alessandro0003 <alessandroquinta3@gmail.com>
2024-01-28 03:14:52 -0300

    :recycle: refactor: rename param is (request.body) a to request.query

--- src/http/controllers/gyms/nearby.ts
+++ src/http/controllers/gyms/nearby.ts
@@ -14,7 +14,7 @@ export async function nearby(request: FastifyRequest, reply: FastifyReply) {
     }),
   })
 
-  const { latitude,  longitude } = nearbyGymsQuerySchema.parse(request.body)
+  const { latitude,  longitude } = nearbyGymsQuerySchema.parse(request.query)
 
   const featchnearbyGymsUseCase = makeFetchNearbyGymsUseCase()     
 
