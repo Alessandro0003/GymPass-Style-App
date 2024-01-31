a8c336ec01e649caa263f32e1b85457233a54841
Alessandro0003 <alessandroquinta3@gmail.com>
2024-01-31 02:31:31 -0300

    :sparkles: feat: create is middlware verifyUserRole

---
+++ src/http/middlweares/verify-user-role.ts
@@ -0,0 +1,11 @@
+import { FastifyReply, FastifyRequest } from "fastify";
+
+export function VerifyUserRole (roleToVerify: 'ADMIN' | 'MEMEBER'){
+    return async (request: FastifyRequest, reply: FastifyReply) => {
+        const { role } = request.user
+        
+        if (role !== roleToVerify ){
+            return reply.status(401).send({ message: 'Unauthorized.' })
+        } 
+    }
+}
