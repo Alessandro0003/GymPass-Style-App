651f0a6ee31a597c576aa24cf004fd9619e84ec3
Alessandro0003 <alessandroquinta3@gmail.com>
2024-01-31 02:32:51 -0300

    :sparkles: feat: add is createAndAuthenticationUser a role

--- src/utils/test/create-and-authentication-user.ts
+++ src/utils/test/create-and-authentication-user.ts
@@ -1,11 +1,16 @@
 import request from 'supertest'
 import { FastifyInstance } from 'fastify'
+import { prisma } from '@/lib/prisma'
+import { hash } from 'bcryptjs'
 
-export async function createAndAuthenticationUser(app: FastifyInstance){
-    await request(app.server).post('/users') .send({
-        name: 'Ale Brilhante',
-        email: 'ale@example.com',
-        password: '123456',
+export async function createAndAuthenticationUser(app: FastifyInstance, isAdmin = false ){
+    await prisma.user.create({
+        data: {
+            name: 'Ale',
+            email: 'ale@example.com',
+            password_hash: await hash('123456', 6),
+            role: isAdmin ? 'ADMIN' : 'MEMBER',
+        }
     })
 
     const authResponse= await request(app.server).post('/sessions').send({
