1ea0d3d05aa15a8eb6079b71dfabe461bf99383d
Alessandro0003 <alessandroquinta3@gmail.com>
2024-01-31 01:18:04 -0300

    :sparkles: feat: ensure is return a refreshToken for the user who as an invalid token

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -3,11 +3,15 @@ import { register } from './register'
 import { authentication } from './authentication'
 import { profile } from './profile'
 import { verifyJWT } from '@/http/middlweares/verify-jwt'
+import { refresh } from './refresh'
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authentication)
 
-  /* Rotas que será executada quando o usuario tiver Authenticated */
+  /* Rota que será executada quando o usuario tiver Authenticated */
   app.get('/me', { onRequest: [verifyJWT] }, profile)
+
+  /* Rota que será executada quando o usuario perder a Authenticated, quando o token não for mais valido*/
+  app.patch('/token/refresh', refresh)
 }
