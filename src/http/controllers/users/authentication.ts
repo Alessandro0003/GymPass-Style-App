c1254f09911cc0cfe16e1d204eadf5e9702c62cf
Alessandro0003 <alessandroquinta3@gmail.com>
2024-01-31 02:30:27 -0300

    :sparkles: feat: ensure that authentication returns an AccessToken to the Admin user or if they are a Member user

--- src/http/controllers/users/authentication.ts
+++ src/http/controllers/users/authentication.ts
@@ -19,14 +19,22 @@ export async function authentication(request: FastifyRequest, reply: FastifyRepl
       password,
     })
 
-    const token = await reply.jwtSign({}, {
+    const token = await reply.jwtSign(
+      {
+        role: user.role
+      },
+
+      {
       sign: {
         sub: user.id,
-      }
+      },
     })
 
     const refreshToken = await reply.jwtSign(
-      {},
+      {
+        role: user.role
+      },
+
       {
         sign: {
           sub: user.id,
